5bce94376e1f1ee1c6c6ee5eed1774b243104ae7
Patrick <nyrud96@gmail.com>
2017-09-30 19:20:59 +0200

    Added stuff

--- mus.py
+++ mus.py
@@ -1,24 +1,52 @@
 from __future__ import unicode_literals
 import youtube_dl
+import pafy
+import os
 from gmusicapi import Musicmanager
 
-def yt_dl():
-	ydl_opts = {
-	    'format': 'bestaudio/best',
-	    'outtmpl': "%(title)s.%(ext)s",
-	    'postprocessors': [{
-	        'key': 'FFmpegExtractAudio',
-	        'preferredcodec': 'mp3',
-	        'preferredquality': '192',
-	    }],
-	}
-	with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-	    ydl.download(['https://www.youtube.com/watch?v=JAr2rUbgPzE'])
+class gui:
+	def main(self):
+		pass
 
-def google_manger():
-	mm = Musicmanager()
-	mm.perform_oauth()
+	def window(self):
+		pass
+
+
+class googleUploader:
+	def main(self):
+		self.ut_url = raw_input("Enter a youtube link: ")
+		self.yt_dl(self.ut_url)
+
+	def yt_dl(self, url):
+		self.video = pafy.new(url)
+		self.vid_name = self.video.title + ".mp3"
+
+		self.ydl_opts = {
+			'format': 'bestaudio/best',
+			'outtmpl': "%(title)s.%(ext)s",
+			'postprocessors': [{
+				'key': 'FFmpegExtractAudio',
+				'preferredcodec': 'mp3',
+				'preferredquality': '192',
+			}],
+		}
+		with youtube_dl.YoutubeDL(self.ydl_opts) as self.ydl:
+			self.ydl.download([url])
+
+
+		self.google_mngr(self.vid_name)
+
+	# def google_strt():
+	# 	mm = Musicmanager()
+	# 	mm.perform_oauth()
+
+	def google_mngr(self, name):
+		self.mm = Musicmanager()
+		self.mm.login()
+
+		self.mm.upload(name, enable_matching=False, enable_transcoding=True, transcode_quality=u'320k')
+		
 
 if __name__ == "__main__":
-	#yt_dl()
-	google_manger() 
+	st = googleUploader()
+	st.main()
