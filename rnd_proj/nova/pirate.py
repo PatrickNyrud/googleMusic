ec2d4313209500446c4061be52f2fe4262032903
Patrick <nyrud96@gmail.com>
2017-11-09 23:32:39 +0100

    nova stuff

--- rnd_proj/nova/pirate.py
+++ rnd_proj/nova/pirate.py
@@ -21,23 +21,37 @@ class Example(tk.Frame):
 
 		self.add_imgs()
 
+
 	def add_imgs(self):
-		self.inv_list = [["Atomic", "199"], ["Commando", "499"], ["Crossfire", "599"]]
+		self.inv_list = [["Atomic", "199"], ["Superti", "329"], ["Magnum", "499"], ["xtrem", "1599"], ["Flashlight", "599"]
+		, ["Phoenix", "599"], ["Bolero", "599"], ["Circus", "599"], ["Bizarre", "599"], ["Goldfish", "599"]
+		, ["Orion", "599"], ["Trapez", "599"], ["Passion", "599"], ["Tnt", "599"], ["Tbird", "599"]
+		, ["Commando", "599"], ["Shocker", "599"], ["Gladiator", "599"], ["Crossfire", "599"], ["Firestorm", "599"]
+		, ["Dragonstep", "599"], ["Hellfire", "599"], ["Diablo", "599"], ["Kickass", "599"], ["Vipblackline", "599"], ["Monster", "599"]
+		, ["Eagle", "599"], ["Monsterpack", "599"], ["Nighthawk", "599"], ["Strobe", "599"], ["Thunderbolt", "599"]
+		, ["Topflight", "599"], ["Partymix", "599"], ["Smatt", "599"], ["Stjerneskudd", "599"], ["Stjerneskuddmini", "599"]
+		, ["Stormlighter", "599"], ["Tikroner", "599"], ["Lyxfontene", "599"], ["Handfakkel", "599"], ["Fargefontene", "599"]]
 		self.func_list = []
 		self.y_pos = 0
-		for row in range(50):
+		self.img_pos = -1
+		for row in range((len(self.inv_list) / 3) + 1):
 			self.y_pos += 1
 			self.x_pos = 0
-			for colums in range(3):
-				self.x_pos += 1
-				self.func_list.append(self.populate(self.inv_list[colums][0], self.inv_list[colums][1], self.x_pos, self.y_pos))
+			try:
+				for colums in range(3):
+					self.img_pos += 1
+					self.x_pos += 1
+					self.func_list.append(self.populate(self.inv_list[self.img_pos][0], self.inv_list[self.img_pos][1], self.x_pos, self.y_pos))
+			except:
+				pass
+
 
 	def populate(self, name, price, x_pos, y_pos):
 		self.btn = tk.Button(self.frame, height = 1, width = 5, command = lambda : self.add(name, price))
 		self.photo = ImageTk.PhotoImage(file = name + "_liten.jpg")
-		self.btn.config(image = self.photo, width = 50, height = 50)
+		self.btn.config(image = self.photo, width = 150, height = 150)
 		self.btn.image = self.photo
-		self.btn.grid(row = y_pos, column = x_pos)
+		self.btn.grid(row = y_pos, column = x_pos, padx = 50, pady = 50)
 
 	def add(self, name, price):
 		print name + " " + price
--- rnd_proj/rnd.py
+++ rnd_proj/rnd.py
@@ -1,10 +1,11 @@
+inv_list = [["Atomic", "199"], ["Superti", "329"], ["Magnum", "499"], ["xtrem", "1599"], ["Flashlight", "599"]
+, ["Phoenix", "599"], ["Bolero", "599"], ["Circus", "599"], ["Bizarre", "599"], ["Goldfish", "599"]
+, ["Orion", "599"], ["Trapez", "599"], ["Passion", "599"], ["Tnt", "599"], ["Tbird", "599"]
+, ["Commando", "599"], ["Shocker", "599"], ["Gladiator", "599"], ["Crossfire", "599"], ["Firestorm", "599"]
+, ["Dragonstep", "599"], ["Hellfire", "599"], ["Diablo", "599"], ["Kickass", "599"], ["Vipblackline", "599"], ["Monster", "599"]
+, ["Eagle", "599"], ["Monsterpack", "599"], ["Nighthawk", "599"], ["Strobe", "599"], ["Thunderbolt", "599"]
+, ["Topflight", "599"], ["Partymix", "599"], ["Smatt", "599"], ["Stjerneskudd", "599"], ["Stjerneskuddmini", "599"]
+, ["Stormlighter", "599"], ["Tikroner", "599"], ["Lyxfontene", "599"], ["Handfakkel", "599"], ["Fargefontene", "599"]]
 
-liste = [["Atomic", "199"], ["Commando", "499"], ["Crossfire", "599"]]
 
-
-
-def pr(name, price):
-	print name + " " + price
-
-for x in range(len(liste)):
-	pr(liste[x][0], liste[x][1])
+print len(inv_list) / 3
