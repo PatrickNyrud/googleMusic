3098ca5a077ea18055a1e4447b2b70592da3a4cf
Patrick <nyrud96@gmail.com>
2018-01-09 21:13:07 +0100

    Minor fixes

--- nova/nova_layout.py
+++ nova/nova_layout.py
@@ -14,18 +14,18 @@ import time
 
 class Nova():
 	def __init__(self):
-		self.x = 800
-		self.y = 800
-		self.check_out_width = 200 #Tune this when change geo size
+		self.x = 1000
+		self.y = 1000
+		self.check_out_width = 250 #Tune this when change geo size
 		self.top_frame_height = 20 #Tune this when change geo size
 		root = tk.Tk()
-		#root.geometry(str(self.x) + "x" + str(self.y))
-		root.attributes("-fullscreen", True)
+		root.geometry(str(self.x) + "x" + str(self.y))
+		#root.attributes("-fullscreen", True)
 
 		self.top_frame = tk.Frame(root, bg = "black", height = self.top_frame_height, width = self.y)
-		self.check_out = tk.Frame(root, background="red", width = self.check_out_width, height = self.x - self.top_frame_height)
+		self.check_out = tk.Frame(root, background="green", width = self.check_out_width, height = self.x - self.top_frame_height)
 		self.main_canvas = tk.Canvas(root, background="yellow")
-		self.main_frame = tk.Frame(self.main_canvas, bg = "green")
+		self.main_frame = tk.Frame(self.main_canvas, bg = "red")
 		self.vsb = tk.Scrollbar(root, orient="vertical", command=self.main_canvas.yview)
 		self.main_canvas.configure(yscrollcommand=self.vsb.set)
 
@@ -34,13 +34,10 @@ class Nova():
 		self.top = tk.Label(self.top_frame, text = "TOP")
 		self.top.place(relx = .5, rely = .5, anchor = "center")
 
-		self.check = tk.Label(self.check_out, text = "CHECK OUT")
-		self.check.place(relx = .5, rely = .5, anchor = "center")
-
-		self.main = tk.Label(self.main_frame, text = "MAIN")
-		self.main.place(relx = .5, rely = .5, anchor = "center")
-
 		#-----------------------------TEMP STUFF-----------------------------#
+
+		self.check_out_label = tk.Label(self.check_out, text = "")
+		self.check_out_label.place(relx = .5, rely = .5, anchor = "center")
 
 		self.top_frame.pack(side = "top", fill = "both")	
 		self.check_out.pack(side = "left", expand = False) 
@@ -55,7 +52,7 @@ class Nova():
 		self.display_text = tkFont.Font(family = "Helvetica", size = 35)
 		self.window_text = tkFont.Font(family = "Helvetica", size = 12)
 
-		self.inv_list = [["Atomic", "199"], ["Super 10", "349"], ["Magnum", "449"], ["Bolero", "1199"], ["Circus", "1199"], ["Bizarre", "1199"]
+		self.inventory_price_list = [["Atomic", "199"], ["Super 10", "349"], ["Magnum", "449"], ["Bolero", "1199"], ["Circus", "1199"], ["Bizarre", "1199"]
 		, ["Goldfish", "1199"], ["Orion", "1199"], ["Trapez", "1199"], ["Passion", "1199"], ["Tnt", "1199"], ["Thunderbird", "1199"]
 		, ["Commando", "1199"], ["Shocker", "1199"], ["Kamikaze", "1199"], ["Gladiator", "1199"]
 		, ["Crossfire", "1199"], ["Black Spider", "1199"], ["Firestorm", "1199"], ["Hellfire", "1199"]]
@@ -85,14 +82,19 @@ class Nova():
 
 		self.item_pos = 0
 		self.frame_position = 0
-		for row in range((len(self.inv_list) / self.column) + 1):
+		for row in range((len(self.inventory_price_list) / self.column) + 1):
 			try: #for loop is longer than list, therefore we need a try to escape the error
 				for column in range(self.column):
+
+					#-------------------------------MAKE FUNC FOR THIS-------------------------------#
 					self.frame = tk.Canvas(self.main_frame, bg = "black", height = 200, width = 200)
 					self.frame.grid(row = row, column = column, padx = (57, 0), pady = (50, 0))
 					self.frame_list.append(self.frame)
-					self.item_name = self.inv_list[self.item_pos][0]
-					self.item_price = self.inv_list[self.item_pos][1]
+					#-------------------------------MAKE FUNC FOR THIS-------------------------------#
+
+					self.item_name = self.inventory_price_list[self.item_pos][0]
+					self.item_price = self.inventory_price_list[self.item_pos][1]
+
 					self.item_pos += 1
 					for x in range(self.item_in_frame):
 						if x == 0:
@@ -103,11 +105,18 @@ class Nova():
 							pass
 						elif x == 1:
 							#THe add button
+							
+							#-----------------------------Change this to like place amount and place label-----------------------------#
 							self.add_button_func.append(self.place_button_add(self.item_name, self.item_price, self.frame_position))
+							#-----------------------------Change this to like place amount and place label-----------------------------#
+						
 						elif x == 2:
 							#The - button
+
+							#-----------------------------Change this to like place amount and place label-----------------------------#
 							self.minus_button_func.append(self.place_button_minus(self.item_name, self.item_price, self.frame_position))
-							pass
+							#-----------------------------Change this to like place amount and place label-----------------------------#
+							
 						elif x == 3:
 							#The amount of items
 							self.place_amount_label(self.item_name, self.frame_position)
@@ -120,6 +129,42 @@ class Nova():
 			except:
 				pass
 
+	def lager(self, name, remove):
+		self.inventory_num_list = []
+		#Appends [["Atomic", 2], ["Superti", 2]] and so on to the list
+		with open("logs//lager.txt", "r") as f:
+			for x in f:
+				self.tmp_var = x.split(",")
+				self.inventory_num_list.append(self.tmp_var)
+		f.close()
+
+		#Remove one from the list, Atomic 2 to Atomic 1
+		#X is the number, and J is the name in the list
+		for x, j in enumerate(self.inventory_num_list):
+			#print "J0 = " + j[0] + " : Name is " + name
+			#added name == J[0] + "," because some items in the list have a , added at the end
+			if j[0] == name or name == j[0] + ",":
+				if remove:
+					self.new_value = int(j[1])
+					self.new_value -= 1
+					self.inventory_num_list[x][0] = j[0] + ", "
+					self.inventory_num_list[x][1] = str(self.new_value) + "\n"
+				else:
+					#print self.inventory_num_list[x]
+					self.inventory_num_list[x][0] = j[0] + ","
+					#print self.inventory_num_list[x][0]
+					self.return_inv_num = self.inventory_num_list[x][1]
+			else:
+				self.inventory_num_list[x][0] = j[0] + ","
+
+			with open("logs//lager.txt", "w+") as f:
+				for x in self.inventory_num_list:
+					for j in x:
+						f.write(j)
+		if not remove:
+			#print self.return_inv_num
+			return self.return_inv_num
+
 	def place_label_lager(self, name, frame_pos):
 		self.label_text = self.lager(name, False).strip()
 		self.label_var = tk.Label(self.frame_list[frame_pos], text = self.label_text)
@@ -129,14 +174,30 @@ class Nova():
 
 
 	def place_amount_label(self, name, frame_pos):
-		self.amount_var = tk.Label(self.frame_list[frame_pos], text = "x2")
+		self.amount_var = tk.Label(self.frame_list[frame_pos], text = "")
 		self.amount_var.place(relx = .8, rely = .7, anchor = "center")
 
 		self.amount_list.append(self.amount_var)
 
-	def change_amount(self, name, frame_pos):
+	#If it goes back to x0, remove it all
+	def change_amount(self, name, frame_pos, add):
 		self.change_var = self.amount_list[frame_pos]
-		self.change_var.config(text = "hgi")
+		self.change_text = self.change_var.cget("text")
+		if len(self.change_text) < 2:
+			self.change_var.config(text = "x1")
+		else:
+			if add:
+				self.change_text_int = int(self.change_text.strip("x"))
+				self.change_text_int += 1
+				self.change_var.config(text = "x" + str(self.change_text_int))
+			else:
+				if self.change_text == "x1":
+					self.change_var.config(text = "")
+				else:
+					self.change_text_int = int(self.change_text.strip("x"))
+					self.change_text_int -= 1
+					self.change_var.config(text = "x" + str(self.change_text_int))
+
 
 	def place_button_add(self, name, price, frame_pos):
 		# self.btn = tk.Button(self.frame, height = 5, width = 15, command = lambda : self.add(name, price), bg = "white")
@@ -144,56 +205,26 @@ class Nova():
 		# self.btn.config(image = self.photo, width = 150, height = 150)
 		# self.btn.image = self.photo
 		# self.btn.grid(row = y_pos, column = x_pos, padx = 50, pady = 50)
-		self.button_place = tk.Button(self.frame_list[frame_pos], text = "ADD", bg = "red",  command = lambda : self.add_sum(name, price))
+		self.button_place = tk.Button(self.frame_list[frame_pos], text = "ADD", bg = "red",  command = lambda : [self.add_sum(name, price), self.change_amount(name, frame_pos, True), self.add_to_checkout(name, price, frame_pos)])
 		self.button_place.place(relx = .5, rely = .5, anchor = "center")
 		#self.text.grid(row = 0, column = 0)
 
 	def place_button_minus(self, name, price, frame_pos):
-		self.button_place = tk.Button(self.frame_list[frame_pos], text = "MINUS", bg = "red",  command = lambda : [self.minus_sum(name, price), self.change_amount(name, frame_pos)])
+		self.button_place = tk.Button(self.frame_list[frame_pos], text = "MINUS", bg = "red",  command = lambda : [self.minus_sum(name, price), self.change_amount(name, frame_pos, False)])
 		self.button_place.place(relx = .5, rely = .7, anchor = "center")
-
-	def lager(self, name, remove):
-		self.inv_list = []
-		#Appends [["Atomic", 2], ["Superti", 2]] and so on to the list
-		with open("logs//lager.txt", "r") as f:
-			for x in f:
-				self.tmp_var = x.split(",")
-				self.inv_list.append(self.tmp_var)
-		f.close()
-
-		#Remove one from the list, Atomic 2 to Atomic 1
-		#X is the number, and J is the name in the list
-		for x, j in enumerate(self.inv_list):
-			#print "J0 = " + j[0] + " : Name is " + name
-			#added name == J[0] + "," because some items in the list have a , added at the end
-			if j[0] == name or name == j[0] + ",":
-				if remove:
-					self.new_value = int(j[1])
-					self.new_value -= 1
-					self.inv_list[x][0] = j[0] + ", "
-					self.inv_list[x][1] = str(self.new_value) + "\n"
-				else:
-					#print self.inv_list[x]
-					self.inv_list[x][0] = j[0] + ","
-					#print self.inv_list[x][0]
-					self.return_inv_num = self.inv_list[x][1]
-			else:
-				self.inv_list[x][0] = j[0] + ","
-
-			with open("logs//lager.txt", "w+") as f:
-				for x in self.inv_list:
-					for j in x:
-						f.write(j)
-		if not remove:
-			#print self.return_inv_num
-			return self.return_inv_num
 
 
 	def add_sum(self, name, price):
 		print name + " " + price
 
+	def add_to_checkout(self, name, price, frame_pos):
+		self.check_out_label.config(text = name + " " + price)
+
 	def minus_sum(self, name, price):
 		print name + " " + price
+
+	def remove_from_checkout(self, name, price, frame_pos):
+		pass
 
 if __name__ == "__main__":
 	strt = Nova()
