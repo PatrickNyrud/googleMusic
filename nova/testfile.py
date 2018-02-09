d021add5460a5281772a1efa365d19b16f33f69a
Patrick <nyrud96@gmail.com>
2018-02-09 13:59:11 +0100

    rnd inv stuff

--- nova/testfile.py
+++ nova/testfile.py
@@ -5,45 +5,27 @@ class test_class:
 		with open("logs//lager.txt", "r") as f:
 			for x in f:
 				self.tmp_var = x.split(",")
-				self.tmp_var[1] = self.tmp_var[1].strip()
 				self.inventory_num_list.append(self.tmp_var)
-
 		f.close()
 
-		#print self.inventory_num_list
-
 		if remove:
-			#In progress
-			for x in self.inventory_num_list:
-				print x
+			self.ch_remove = "'[]"
+			for j, x in enumerate(self.inventory_num_list):
+				if x[0] in name:
+					self.remove_inv_sum = self.inventory_num_list[j][1]
+					self.new_sum = int(self.remove_inv_sum) - 1
+					self.inventory_num_list[j][1] = str(self.new_sum) + "\n"
+					with open("logs//lager.txt", "w") as f:
+						for x in self.inventory_num_list:
+							x[1] = x[1].strip(" ")
+							self.final_string = str(x)
+							for c in self.ch_remove:
+								self.final_string = self.final_string.replace(c, "")
+							f.write(self.final_string.replace("\\n", "\n"))
 		else:
-			#Done Works
 			for x in self.inventory_num_list:
 				if x[0] == name:
 					return x[1]
 
-		# #Remove one from the list, Atomic 2 to Atomic 1
-		# #X is the number, and J is the name in the list
-		# for x, j in enumerate(self.inventory_num_list):
-		# 	#added name == J[0] + "," because some items in the list have a , added at the end
-		# 	if j[0] == name or name == j[0] + "," or name == "Super 10 (349)":
-		# 		if remove:
-		# 			self.new_value = int(j[1])
-		# 			self.new_value -= 1
-		# 			self.inventory_num_list[x][0] = j[0] + ", "
-		# 			self.inventory_num_list[x][1] = str(self.new_value) + "\n"
-		# 		else:
-		# 			self.inventory_num_list[x][0] = j[0] + ","
-		# 			self.return_inv_num = self.inventory_num_list[x][1]
-		# 	else:
-		# 		self.inventory_num_list[x][0] = j[0] + ","
-
-		# 	with open("logs//lager.txt", "w+") as f:
-		# 		for x in self.inventory_num_list:
-		# 			for j in x:
-		# 				f.write(j)
-		# if not remove:
-		# 	return self.return_inv_num
-
 tt = test_class()
-tt.inventory("Orion", False)
+tt.inventory("Orion", True)
