a13da3d1d7dd14684a7c56e79e7e0fefb4bd045e
Patrick <nyrud96@gmail.com>
2018-03-13 13:20:57 +0100

    cpp stuff

--- cpp/hangman.cpp
+++ cpp/hangman.cpp
@@ -1,17 +1,47 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 bool checkIfRight(string, string);
+void runHangman(void);
+void testFunc(void);
 
 int main(){
 	system("cls");
+	
+	testFunc();
+	
+	return 0;
+}
+
+void testFunc(void){
+	vector<char> liste;
+	bool done = false;
+	char guess;
+
+	while(! done){
+		cin >> guess;
+		if(find(liste.begin(), liste.end(), guess) != liste.end()){
+			cout << "Youve guessed " << guess << endl;
+		} else{
+			liste.push_back(guess);
+		}
+
+		for(int i = 0; i < liste.size(); i++){
+			cout << "at " << i << " is " << liste[i] << endl;
+		}
+	}
+}
+
+void runHangman(void){
 	string guess;
 	string word = "guess";
 	bool done = false;
 	int attempts = 1;
 
-	cout << "Guess the word" << endl;
+	cout << "Guess the word" << endl << "> ";
 
 	while(! done){
 		cin >> guess;
@@ -21,13 +51,13 @@ int main(){
 			cout << "You got it in " << attempts << " attempts!" << endl;
 		} else{
 			system("cls");
-			cout << "Not right" << endl;
+			cout << "Not right, guess again" << endl << "> ";
 		}
 		attempts++;
 	}
-	return 0;
-}
 
+
+}
 
 bool checkIfRight(string guess, string word){
 	if(guess == word){
